9002417788dd88b06af3647ec43fb6dc2fae7bef
Maximilian Weber <weber.maximilian@googlemail.com>
2011-11-12 11:30:06 +0100

    Version corrected.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject mongodb-event-store "1.0.0-SNAPSHOT"
+(defproject mongodb-event-store "0.1.0-SNAPSHOT"
   :description "An event store for MongoDB."
   :dependencies [[org.clojure/clojure "1.2.1"]
                  [congomongo "0.1.4-SNAPSHOT"]
